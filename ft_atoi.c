5b2d1339f372f107e4ec837dc955d4c516ff0fa5
Yassein Eltayeb Mohamed Idris <107728160+yelsama@users.noreply.github.com>
2022-07-29 16:33:44 +0400

    long long int max fixed

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,13 @@
 /*   By: ymohamed <ymohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 23:30:18 by ymohamed          #+#    #+#             */
-/*   Updated: 2022/07/29 03:10:14 by ymohamed         ###   ########.fr       */
+/*   Updated: 2022/07/29 16:30:01 by ymohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 static void	calcu(int *nb, int *md, int *i, char *ptr);
 
@@ -34,19 +38,42 @@ int	ft_atoi(const char *str)
 		i++;
 	}
 	calcu(&nb, &md, &i, ptr);
-	if (md >= 19 && sn < 0)
+	if (md > 19 && sn < 0)
 		return (0);
-	else if (md >= 19 && sn > 0)
+	else if (md > 19 && sn > 0)
 		return (-1);
 	return (nb * sn);
 }
 
 static void	calcu(int *nb, int *md, int *i, char *ptr)
 {
-	while (ptr[*i] >= '0' && ptr[*i] <= '9' && *md < 20)
+	char	*max;
+	int		c;
+	int		m;
+
+	c = *i;
+	m = 0;
+	max = "9223372036854775807";
+	while (ptr[*i] >= '0' && ptr[*i] <= '9' && *md < 21)
 	{
 		*nb = *nb * 10 + (ptr[*i] - 48);
 		*md = *md + 1;
 		*i = *i + 1;
 	}
+	if (*md == 19)
+	{
+		while (ptr[c] <= max[m] && m <= 19)
+		{
+			m++;
+			c++;
+		}
+		if (m < 19)
+			*md = 20;
+	}
 }
+// int	main(void)
+// {
+// 	printf("%d\n", ft_atoi("-9223372036854775808")); //9223372036854775807
+// 	printf("%d\n", atoi("-9223372036854775808"));
+// 	return (0);
+// }
